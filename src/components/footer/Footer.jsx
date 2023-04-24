eadc1545c1e315e6741a3c03e696e41b61fdfaf6
TorezC <victorytorez@gmail.com>
2023-04-24 02:38:52 +0100

    updated the footer and created the about page

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,6 @@
-import { NavLink } from 'react-router-dom';
-import './footer.css';
+import { NavLink } from "react-router-dom";
+import kamex from '../../assets/kamex.jpeg'
+import "./footer.css";
 
 const Footer = () => {
   return (
@@ -7,54 +8,74 @@ const Footer = () => {
       <section class="contact-section">
         <div class="container">
           <div class="row">
+          <div class="col-lg-3 col-md-6">
+              <div class="contact-title">
+                <img src={kamex} alt='logo'  width={100} height={100} className='img-fluid' />
+              </div>
+              <ul class="contact-link p-0 mt-3">
+                <li>
+                  <p className="footer-content">Copyright © Kamex Group 2023</p>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-title">
+                <h4>Company</h4>
+              </div>
+              <ul class="contact-link p-0">
+                <li>
+                  <NavLink to="/about-us">About Us</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/team">Team</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </div>
+            <div class="col-lg-3 col-md-6">
+              <div class="contact-title">
+                <h4>Follow Us</h4>
+              </div>
+              <ul class="contact-link p-0">
+                <li className="footer-social text-right">
+                  <a href="/#">
+                    <i class="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="/#">
+                    <i class="fab fa-twitter"></i>
+                  </a>
+                  <a href="/#">
+                    <i class="fab fa-google"></i>
+                  </a>
+                  <a href="/#">
+                    <i class="fab fa-instagram"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
             <div class="col-lg-3 col-md-6">
               <div class="contact-title">
                 <h4>Contact Us</h4>
               </div>
-              <ul class="contact-link">
+              <ul class="contact-link p-0">
                 <li>
                   <a href="tel:+2348104025253">+2348104025253</a>
                 </li>
                 <li>
-                  <a href="mailto:victorytorez@gmail.com">info@kamexgroup.com</a>
+                  <a href="mailto:victorytorez@gmail.com">
+                    info@kamexgroup.com
+                  </a>
                 </li>
                 <li>
                   <a href="/">90 Circular Road Freetown, Sierra Leone</a>
                 </li>
               </ul>
             </div>
-            <div class="col-lg-3 col-md-6">
-              <div class="contact-title">
-                <h4>About Us</h4>
-              </div>
-              <ul class="contact-link">
-                <li>
-                  <NavLink to='/about-us'>Who we are</NavLink>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
       </section>
-      <footer class="footer-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 col-md-6">
-                        <div class="footer-content">
-                            <p>Copyright © 2023</p>
-                        </div>
-                    </div>
-                    <div class="col-lg-6 col-md-6">
-                        <div class="footer-social text-right">
-                            <a href="/#"> <i class="fab fa-facebook-f"></i></a>
-                            <a href="/#"><i class="fab fa-twitter"></i></a>
-                            <a href="/#"><i class="fab fa-google"></i></a>
-                            <a href="/#"><i class="fab fa-instagram"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
     </div>
   );
 };
